5678e48cfe37e8ad6cb6e58ca4a880fcd7a60836
Kreezxil <kreezxil@gmail.com>
2017-09-08 07:56:44 -0500

    initial logo work

--- src/main/java/com/kreezcraft/woodstone/Woodstone.java
+++ src/main/java/com/kreezcraft/woodstone/Woodstone.java
@@ -5,11 +5,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = Woodstone.MODID, version = Woodstone.VERSION)
+@Mod(modid = Woodstone.MODID, version = Woodstone.VERSION, name = Woodstone.NAME)
 public class Woodstone
 {
     public static final String MODID = "woodstone";
     public static final String VERSION = "1.12.1-1.1";
+    public static final String NAME = "Woodstone";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
